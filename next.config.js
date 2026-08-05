5894b3c17e185f48de5dcb739672e419717efadb
vann10 <irvnurikh@Gmail.com>
2026-08-05 20:47:11 +0700

    minor update: fix pdf export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["pdfkit", "fontkit"],
+  },
   async redirects() {
     return [
       {
